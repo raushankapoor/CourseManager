19793cd80a8217394b430930765c2f908230e235
Raushan <kapoor.1@iitj.ac.in>
2021-07-21 14:18:02 +0530

    Created models for course and wishlist

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.name
+
+
+class WishList(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    course = models.ForeignKey(Course,on_delete=models.CASCADE)
+
