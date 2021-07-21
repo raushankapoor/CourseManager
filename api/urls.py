7061533f30b2a8f3a1788cfb6eaa029c9244fc48
Raushan <kapoor.1@iitj.ac.in>
2021-07-21 16:07:36 +0530

    added the apis for course model

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-  path('welcome', views.welcome)
+  path('welcome', views.welcome),
+  path('getcourses', views.get_courses),
+  path('addcourse', views.add_course),
+  path('deletecourse/<int:course_id>', views.delete_course)
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,14 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from django.http import JsonResponse
+from django.http import JsonResponse,response
 from django.views.decorators.csrf import csrf_exempt
+from .serializers import CourseSerializer
+from .models import Course,WishList
+from rest_framework import status
+import json
+from django.core.exceptions import ObjectDoesNotExist
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 @api_view(["GET"])
 @csrf_exempt
@@ -9,3 +16,57 @@ from django.views.decorators.csrf import csrf_exempt
 def welcome(request):
     content = {"message": "Welcome to the Couser Manager!"}
     return JsonResponse(content)
+
+@api_view(["GET"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def get_courses(request):
+    courses = Course.objects.all()
+    serializer = CourseSerializer(courses, many=True)
+    return JsonResponse({'courses': serializer.data}, safe=False, status=status.HTTP_200_OK)
+
+@api_view(["POST"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def add_course(request):
+    User = get_user_model()
+    payload = json.loads(request.body)
+    user = request.user
+    try:
+        name = payload["name"]
+        author = payload["author"]
+        price = float(payload["price"])
+        print("price good")
+        users_ids = payload["users"]
+        print("user good")
+        course = Course(name=name,author=author,price=price)
+        course.save()
+        print(users_ids)
+        for userid in users_ids:
+            print(userid)
+            user = User.objects.filter(id=int(userid)).first()
+            print(user)
+            course.users.add(user)
+        course.save()
+        serializer = CourseSerializer(course)
+        return JsonResponse({'course': serializer.data}, safe=False, status=status.HTTP_201_CREATED)
+    except ObjectDoesNotExist as e:
+        return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
+    except Exception:
+        return JsonResponse({'error': 'Something went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+@api_view(["DELETE"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def delete_course(request, course_id):
+    user = request.user.id
+    try:
+        course = Course.objects.get(id=course_id)
+        course.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except ObjectDoesNotExist as e:
+        return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
+    except Exception:
+        return JsonResponse({'error': 'Something went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
